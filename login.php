0f4a317879366202268eeb5b9cc0fb30b33b44b2
Ralph Mariano <61544873+raftaaan@users.noreply.github.com>
2022-10-05 21:37:50 +0800

    Add files via upload

--- login.php
+++ login.php
@@ -1,63 +1,103 @@
 <?php
-require_once $_SERVER['DOCUMENT_ROOT'].'/test/resource/php/class/core/init.php';
-
- ?>
+require_once $_SERVER['DOCUMENT_ROOT'].'/cave/resource/php/class/core/init.php';
+?>
 
 <!DOCTYPE html>
 <html lang="en">
-<head>
-  <meta charset="UTF-8">
-  <meta name="viewport" content="width=device-width, initial-scale=1.0">
-  <title>Registrar Portal</title>
-  <link rel="stylesheet" type="text/css"  href="vendor/css/bootstrap.min.css">
-  <link href="vendor/css/all.css" rel="stylesheet">
-  <link rel="stylesheet" type="text/css"  href="resource/css/styles.css">
-</head>
-<body>
-        <nav class="navbar navbar-dark bg-white shadow-sm slide-in-left">
-          <a class="navbar-brand " href="https://malolos.ceu.edu.ph/">
-            <img src="resource/img/logo.jpg" height="70" class="d-inline-block align-top"
-              alt="mdb logo"><h3 class="ib">
-          </a>
-             <a href="https:/www.facebook.com/theCEUofficial/"><i class="fab fa-facebook-f ceucolor"></i></a>
-             <a href="https://www.instagram.com/ceuofficial/"><i class="fab fa-instagram ceucolor"></i></a>
-             <a href="https://twitter.com/ceumalolos"><i class="fab fa-twitter ceucolor"></i></a>
-        </nav>
-           <div class="container mt-5">
-            <div class="row justify-content-center">
-                <div class="col-8 ">
 
+    <head>
+        <meta charset="utf-8" />
+        <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no" />
+        <meta name="description" content="" />
+        <meta name="author" content="" />
+        <title>CEU CAVEPortal</title>
+        <!-- Favicon-->
+        <link rel="icon" type="image/x-icon" href="assets/logo_icon.ico"/>
+        <!-- Bootstrap icons-->
+        <link href="https://cdn.jsdelivr.net/npm/bootstrap-icons@1.5.0/font/bootstrap-icons.css" rel="stylesheet" />
+        <!-- Core theme CSS (includes Bootstrap)-->
+        <link href="css/login.css" rel="stylesheet" />
+        <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.5.1/jquery.min.js"></script>
+        <script src="https://kit.fontawesome.com/b04d2a2a76.js" crossorigin="anonymous"></script>
 
+        <link rel="preconnect" href="https://fonts.gstatic.com">
+        <link href="https://fonts.googleapis.com/css2?family=Kiwi+Maru:wght@300;500&display=swap" rel="stylesheet">
 
-                    <form class="text-center border border-light p-5 shadow puff-in-center" action="" method="post" >
-                    <p class="h4 mb-4">Sign in</p>
-                    <?php logd();?>
-                    <input type="text" id="username" class="form-control mb-4" name="username" placeholder="Enter Username" required>
-                    <input type="password" id="defaultLoginFormPassword" class="form-control mb-4" placeholder="Enter Password" name ="password" required>
-                    <div class="d-flex justify-content-around">
+    </head>
+    <body class="d-flex flex-column h-100">
+        <main class="flex-shrink-0">
+            <!-- Navigation-->
+            <nav class="navbar navbar-expand-lg navbar-dark bg-dark">
+                <div class="container nav-container px-5">
+                    <a class="navbar-brand">
+                        <img src="resource/img/CAVElogo-white.png" alt="" width="300" height="55" class="img-fluid logo">
+                    <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation"><span class="navbar-toggler-icon"></span></button>
+                    <div class="collapse navbar-collapse" id="navbarSupportedContent">
+                        <ul class="navbar-nav ms-auto mb-2 mb-lg-0">
+                            <li class="nav-item"><a class="nav-link" href="index.html">Home</a></li>
+                            <li class="nav-item"><a class="nav-link" href="about.html">Alumni Verification</a></li>
+                            <li class="nav-item"><a class="nav-link" href="contact.html">Check Status</a></li>
+                            <li class="nav-item dropdown">
+                                <a class="nav-link dropdown-toggle" id="navbarDropdownPortfolio" href="#" role="button" data-bs-toggle="dropdown" aria-expanded="false">My Account</a>
+                                <ul class="dropdown-menu dropdown-menu-end" aria-labelledby="navbarDropdownPortfolio">
+                                    <li><a class="dropdown-item" href="login.html">Log in</a></li>
+                                    <li><a class="dropdown-item" href="register.html">Register</a></li>
+                                </ul>
+                            </li>
+                        </ul>
                     </div>
-                    <input type =hidden name="token" value="<?php echo Token::generate(); ?>">
-                    <input  type="submit"  class="btn btn-dark btn-block my-4"value="Login"/>
-                    </form>
-
-
-                    <footer id="sticky-footer" class="py-4 bg-dark text-white-50 fixed-bottom slide-in-right">
-                      <div class="container text-center">
-                          <div class="row">
-                              <div class="col col-sm-5 text-left">
-                                  <small>Copyright &copy;Centro Escolar University     Office of the Registrar 2019</small>
-                              </div>
-                              <div class="col text-right">
-                                  <small>Created by: Reymart Bolasoc, Amelia Valencia , James Mangalile, Kenneth De Leon , Pamela Reyes , Ellen Mijares</small>
-                              </div>
-                          </div>
-                      </div>
+                </div>
+            </nav>
+            
+            <!-- Header-->
+    <header class="bg-dark py-lg-5">
+        <div class="container">
+                <section class="wrapper">
+                    <div class="container">
+                        <div class="col-sm-8 offset-sm-2 col-lg-6 offset-lg-3 col-xl-4 offset-xl-4 text-center">
+                            <form class="rounded bg-white shadow py-5 px-4">
+                                <h3 class="text-dark fw-bolder fs-4 mb-2">Sign In</h3>
+                                <div class="fw-normal text-muted mb-4"> New Here?
+                                    <a href="#" class="text-primary fw-bold text-decoration-none">Inquire for an Account</a>
+                                </div>
+                                <?php logd();?>
+                                <div class="form-floating mb-3">
+                                    <input type="email" class="form-control" id="floatingInput" placeholder="name@example.com">
+                                    <label for="floatingInput">Email address</label>
+                                </div>
+                                <div class="form-floating">
+                                    <input type="password" class="form-control" id="floatingPassword" placeholder="Password">
+                                    <label for="floatingPassword">Password</label>
+                                </div>
+                                <div class="mt-2 text-end">
+                                    <a href="#" class="text-primary fw-bold text-decoration-none">Forget Password?</a>
+                                </div>
+                                <button type="submit" class="btn btn-primary submit_btn w-100 my-4">Continue</button>
+                            </form>
+                        </div>
+                    </div>
+                </section>
+            z</header>
+            
+        </main>
+        <!-- Footer-->
+        <footer class="bg-dark py-4 mt-auto">
+            <div class="container px-5">
+                <div class="row align-items-center justify-content-between flex-column flex-sm-row">
+                    <div class="col-auto"><div class="small m-0 text-white">Centro Escolar University</div></div>
+                    <div class="col-auto">
+                        <a class="link-light small" href="#!">Privacy</a>
+                        <span class="text-white mx-1">&middot;</span>
+                        <a class="link-light small" href="#!">Terms</a>
+                        <span class="text-white mx-1">&middot;</span>
+                        <a class="link-light small" href="#!">Contact</a>
+                    </div>
                 </div>
             </div>
-          </div>
         </footer>
-        <script src="vendor/js/jquery.js"></script>
-        <script src="vendor/js/popper.js"></script>
-        <script src="vendor/js/bootstrap.min.js"></script>
+        <!-- Bootstrap core JS-->
+        <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.1.3/dist/js/bootstrap.bundle.min.js"></script>
+        <!-- Core theme JS-->
+        <script src="js/scripts.js"></script>
     </body>
-    </html>
+</html>
